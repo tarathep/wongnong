4056b3ed50d71f2e8325f8e7f671e480d517daa2
DESKTOP-ONQ401U\weera <weerazero@gmail.com>
2019-03-18 01:08:06 +0700

    /wee

--- contact.php
+++ contact.php
@@ -28,6 +28,7 @@
 	<link rel="stylesheet" href="css/nouislider.min.css">
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -85,52 +86,58 @@
 	<!--================Contact Area =================-->
 	<section class="contact_area section_gap_bottom">
 		<div class="container">
-			<div id="mapBox" class="mapBox" data-lat="40.701083" data-lon="-74.1522848" data-zoom="13" data-info="PO Box CT16122 Collins Street West, Victoria 8007, Australia."
-			 data-mlat="40.701083" data-mlon="-74.1522848">
+		<div id="mapBox" class="mapBox" data-lat="13.7379482" data-lon="100.62833180000007" data-zoom="15" data-info="Thai-Nichi Institute of Technology"
+		data-mlat="13.7379482" data-mlon="100.62833180000007">
 			</div>
+
+			<div><h2 >Ours Team</h2></div>
 			<div class="row">
-				<div class="col-lg-3">
-					<div class="contact_info">
-						<div class="info_item">
-							<i class="lnr lnr-home"></i>
-							<h6>California, United States</h6>
-							<p>Santa monica bullevard</p>
-						</div>
-						<div class="info_item">
-							<i class="lnr lnr-phone-handset"></i>
-							<h6><a href="#">00 (440) 9865 562</a></h6>
-							<p>Mon to Fri 9am to 6 pm</p>
-						</div>
-						<div class="info_item">
-							<i class="lnr lnr-envelope"></i>
-							<h6><a href="#">support@colorlib.com</a></h6>
-							<p>Send us your query anytime!</p>
-						</div>
-					</div>
+
+			
+
+				<div class= "col-xl-3 col-lg-4 col-md-6 col-sm-6 cards">
+				
+				<div class="image-cropper">
+					<img src="images/tarathep.jpg" class="rounded" />
+					
 				</div>
-				<div class="col-lg-9">
-					<form class="row contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-						<div class="col-md-6">
-							<div class="form-group">
-								<input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'">
-							</div>
-							<div class="form-group">
-								<input type="email" class="form-control" id="email" name="email" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'">
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" id="subject" name="subject" placeholder="Enter Subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'">
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<textarea class="form-control" name="message" id="message" rows="1" placeholder="Enter Message" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'"></textarea>
-							</div>
-						</div>
-						<div class="col-md-12 text-right">
-							<button type="submit" value="submit" class="primary-btn">Send Message</button>
-						</div>
-					</form>
+				<p class="cen">
+				Tarathep Cheongchaiyaphum
+				</p>
+				<p class="cenid">Nickname : Bokie<br> id : 5812111-7<br><a href="">facebook</a></p>
+			   </div>
+
+
+			   
+				<div class= "col-xl-3 col-lg-4 col-md-6 col-sm-6 cards">
+				
+				<div class="image-cropper">
+    				<img src="images/Veerapat.jpg" class="rounded" />
 				</div>
+				<p class="cen">veerapat boonyasing</p>
+				<p class="cenid">Nickname : Poom<br> id : 58121098-6<br><a href="">facebook</a></p>
+			   </div>
+
+
+			   
+				<div class= "col-xl-3 col-lg-4 col-md-6 col-sm-6 cards ">
+				
+				<div class="image-cropper">
+   					 <img src="images/teeraphan.jpg" class="rounded" />
+				</div>
+				<p class="cen">Teeraphan Bunsoemsiri</p>
+				<p class="cenid">Nickname : Aon<br> id : 58121049-9<br><a href="">facebook</a></p>
+			   </div>
+
+			   
+				<div class= "col-xl-3 col-lg-4 col-md-6 col-sm-6 cards ">
+				
+				<div class="image-cropper">
+   					 <img src="images/weerachai.jpg" class="rounded" />
+				</div>
+				<p class="cen">Weerachai Vichienrat</p>
+				<p class="cenid">Nickname : Wee<br> id : 5812114-1<br><a href="">facebook</a></p>
+			   </div>
 			</div>
 		</div>
 	</section>
@@ -218,7 +225,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script>  <i c
 	<script src="js/jquery.magnific-popup.min.js"></script>
 	<script src="js/owl.carousel.min.js"></script>
 	<!--gmaps Js-->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBs2SHS8IS4NPpUX1DImwrOPQPXbKdC8jw"></script>
 	<script src="js/gmaps.min.js"></script>
 	<script src="js/main.js"></script>
 </body>
